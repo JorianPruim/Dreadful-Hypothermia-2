7d725183e86b037734b0229ed7294a94d546ca04
Just_A_Dutchman_ <70569476+JustADutchman@users.noreply.github.com>
2020-10-13 22:13:27 +0200

    Update InformaticaGame.java

--- src/java/InformaticaGame.java
+++ src/java/InformaticaGame.java
@@ -4,7 +4,14 @@ import worldgen.World;
 
 public class InformaticaGame {
   public static void main(String[] args) {
-    System.out.println(new Map(125,10,30,1e-2));
+    Map humidity = new Map(125,10,30,1e-2);
+    Map heat = new Map(125,10,30,1e-2);
+
+    String heatData = world.toString.getMapData(heat.toIntegerDeepArray());
+    String humidityData = world.toString.getMapData(humidity.toIntegerDeepArray());
+
+    System.out.println(heatData);
+    System.out.println(humidityData);
 
   }
 
