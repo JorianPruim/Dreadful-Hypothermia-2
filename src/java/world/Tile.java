6b2ce61c5e5018fdfc58b3cc20c0fcd778bc8f3d
JorianPruim <jorian.pruim@gmail.com>
2020-10-28 14:36:18 +0100

    Update Tile.java

--- src/java/world/Tile.java
+++ src/java/world/Tile.java
@@ -6,5 +6,22 @@ public class Tile {
 
     Building building = null;
     Subterrain sub = null;
+    
+    public void onPrimaryInteract(Player p){
+        if(this.building!=null){
+            this.building.onPrimaryInteract(p);
+        }
+        return;
+    }
+    public void onSecondaryInteract(Player p){
+        if(this.building!=null){
+            this.building.onPrimaryInteract(p);
+        }
+        return;
+    }
+    private void build(Building b){
+        return;
+    }
+    
 
 }
