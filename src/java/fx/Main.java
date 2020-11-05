ed2c32841b8f04d1093bf90e39d6c4935ffc37ea
Rikkie! <rikkiehuls@gmail.com>
2020-11-05 22:15:54 +0100

    tried doing something with GripPane and TilePane, still testing!

--- src/java/fx/Main.java
+++ src/java/fx/Main.java
@@ -3,7 +3,7 @@ package fx;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,15 +19,28 @@ public class Main extends Application {
         Map b = new Map(1000,15,25,0.003);
         Text t = new Text(b.toVisualString());
 
-
+        //testing only
+        int height = 1000;
+        int width = 600;
         primaryStage.setTitle("Hello World");
 
 
-        StackPane pane = new StackPane(t);
+        GridPane pane = new GridPane();
+
+        Text[][] visualMap = new Text[25][25];
+        for(int i = 0; i<visualMap.length; i++){
+            for(int j = 0; j<visualMap.length; j++){
+                visualMap[i][j] = new Text("" + i + j);
+                GridPane.setConstraints(visualMap[i][j], j, i);
+                pane.getChildren().add(visualMap[i][j]);
+            }
+        }
+
+//        GridPane.setConstraints(t, 0, 25);
+//        pane.getChildren().add(t);
 
 
-
-        Scene s = new Scene(pane, 2000, 600);
+        Scene s = new Scene(pane, height, width);
 
         primaryStage.setScene(s);
         primaryStage.show();
